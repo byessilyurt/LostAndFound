caf1fb0d2e7beba25914096b90d33f0c58c14707
byessilyurt <y.yesilyurt14@gmail.com>
2023-04-28 01:31:15 +0200

    item card added

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -1,7 +1,48 @@
 import React from "react";
+import { IoMdTime } from "react-icons/io";
+import { RiErrorWarningLine } from "react-icons/ri";
 
-function ItemCard() {
-  return <div>ItemCard</div>;
-}
+import DropdownMenu from "./DropdownMenu";
+
+const ItemCard = ({ item }) => {
+  const isLost = item.status === "lost";
+  const statusColor = isLost ? "lostColor" : "foundColor";
+
+  return (
+    <div className="relative w-[350px] h-88 bg-white rounded-lg shadow">
+      <img
+        src={item.image}
+        alt={item.title}
+        className="w-full h-40 rounded-t-lg"
+      />
+      <div className="p-4">
+        <div className="flex justify-between items-center mb-2">
+          <h3 className="text-lg font-bold">{item.title}</h3>
+          <DropdownMenu />
+        </div>
+        <div className="flex items-center text-sm font-semibold text-gray-500">
+          <IoMdTime className="mr-1 w-2.5 h-2.5" />
+          <span>{item.timeAgo}</span>
+          <span className={`mx-2 w-1 h-1 rounded-full bg-${statusColor}`} />
+          <span>{item.location}</span>
+        </div>
+      </div>
+      <div
+        className={`inline-flex justify-center items-center gap-1 bg-gray-100 text-sm font-semibold text-gray-700 rounded-full px-4 py-1 max-w-full truncate my-4 mx-4`}
+      >
+        <RiErrorWarningLine className={`mr-1 text-${statusColor} w-3 h-3`} />
+        <span className="truncate">{item.message}</span>
+      </div>
+
+      <div
+        className={`uppercase font-bold px-4 py-1 rounded-bl-lg rounded-tr-lg bg-${statusColor} absolute top-0 right-0 ${
+          item.status.toLowerCase() === "lost" ? "text-black" : "text-white"
+        } text-sm`}
+      >
+        {item.status}
+      </div>
+    </div>
+  );
+};
 
 export default ItemCard;
--- src/components/TabsProfile.js
+++ src/components/TabsProfile.js
@@ -2,6 +2,38 @@ import { useState } from "react";
 
 import AccountDetails from "./AccountDetails";
 import AccountSettings from "./AccountSettings";
+import ItemCard from "./ItemCard";
+import itemImg from "../images/item.png";
+
+const items = [
+  {
+    id: 1,
+    title: "Item 1",
+    image: itemImg,
+    timeAgo: "1 hour ago",
+    location: "Location 1",
+    message: "Message 1",
+    status: "lost",
+  },
+  {
+    id: 2,
+    title: "Item 2",
+    image: itemImg,
+    timeAgo: "2 hours ago",
+    location: "Location 2",
+    message: "Message 2",
+    status: "found",
+  },
+  {
+    id: 3,
+    title: "Item 3",
+    image: itemImg,
+    timeAgo: "3 hours ago",
+    location: "Location 3",
+    message: "Message 3",
+    status: "lost",
+  },
+];
 
 const tabs = [
   {
@@ -10,11 +42,11 @@ const tabs = [
     content: (
       <>
         <h1 className="text-left font-bold text-4xl mb-4">Posts</h1>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 justify-items-center mx-auto">
           {/* Replace the divs below with your actual Post components */}
-          <div className="bg-gray-300 h-24"></div>
-          <div className="bg-gray-300 h-24"></div>
-          <div className="bg-gray-300 h-24"></div>
+          {items.map((item) => (
+            <ItemCard key={item.id} item={item} />
+          ))}
         </div>
       </>
     ),
