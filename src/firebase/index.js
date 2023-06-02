b8002f7ec8b8b0dd7648444875f27ba0e2302efe
raveen-ibrahim <58376884+raveen-ibrahim@users.noreply.github.com>
2023-06-03 01:00:23 +0200

    Add Firebase authentication google, email,  github.

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -15,5 +16,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const db = getFirestore(app);
+const auth = getAuth(app);
 
-export { db };
+export { db,auth };
